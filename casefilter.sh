e18ec1594b5b976fda316a2179968d6a86cd728d
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2016-03-04 21:03:03 +0000

    Add some more files to fix-up

--- casefilter.sh
+++ casefilter.sh
@@ -2,7 +2,7 @@
 #
 # Rewrite git history to manage files that changed case at some point in their
 # SVN history, because this creates a git repository that cannot be used
-# properly on case preserving+insensitive filesystems, such as the default
+# properly on case preserving+insensitive filesystems, which is the default
 # mode for HFS+ filesystems on OS X
 #
 # This script filters all commit indexes and updates the 'bad' case name
@@ -14,10 +14,17 @@ git filter-branch --index-filter '
 git ls-files --stage | \
 sed "s:Tools/RunTimeTester/share/FileComparator.py:Tools/RunTimeTester/share/fileComparator.py:" | \
 sed "s:Tools/RunTimeTester/src/ShellCommand.py:Tools/Tools/RunTimeTester/src/shellcommand.py:" | \
-sed "s:Trigger/TrigHypothesis/TrigJetHypo/src/TrigHLTJetHypoHelpers/CleanerFactory.cxx:Trigger/TrigHypothesis/TrigJetHypo/src/TrigHLTJetHypoHelpers/cleanerFactory.cxx:" | \  # Need to double check this one - not in master/HEAD
+sed "s:Trigger/TrigHypothesis/TrigJetHypo/src/TrigHLTJetHypoHelpers/cleanerFactory.cxx:Trigger/TrigHypothesis/TrigJetHypo/src/TrigHLTJetHypoHelpers/CleanerFactory.cxx:" | \
 sed "s:Trigger/TrigT1/L1Topo/L1TopoAlgorithms/L1TopoAlgorithms/LAR.h:Trigger/TrigT1/L1Topo/L1TopoAlgorithms/L1TopoAlgorithms/LAr.h:" | \
 sed "s:Trigger/TrigT1/L1Topo/L1TopoAlgorithms/Root/LAR.cxx:Trigger/TrigT1/L1Topo/L1TopoAlgorithms/Root/LAr.cxx:" | \
 GIT_INDEX_FILE=$GIT_INDEX_FILE.new \
 git update-index --index-info && \
 mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"
 ' HEAD
+
+
+# List of other possible bad yins (from 20.1.1):
+#
+# Generators/PowhegControl/share/PowhegControl_HWj_Common.py
+# Generators/PowhegControl/share/PowhegControl_HZj_Common.py
+# InnerDetector/InDetValidation/InDetGeometryValidation/Changelog
