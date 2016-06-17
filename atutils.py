4405712f52e25d64f3136a3401646976d115dda3
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2016-06-17 23:41:35 +0200

    Add svn metadata utilities; import necessary modules

--- atutils.py
+++ atutils.py
@@ -15,7 +15,12 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
+import os
+import os.path
+import re
 import subprocess
+import time
 
 from glogger import logger
 
@@ -69,4 +74,27 @@ def author_string(author):
         return "{0} <{0}@cern.ch>".format(author)
     return author
 
+
+def initialise_svn_metadata(svncachefile):
+    ## @brief Load existing cache file, if it exists, or return empty cache
+    #  @param svncachefile Name of svn cache file (serialised in JSON)
+    if os.path.exists(svncachefile):
+        logger.info("Reloading SVN cache from {0}".format(svncachefile))
+        with file(svncachefile) as md_load:
+            svn_metadata_cache = json.load(md_load)
+    else:
+        svn_metadata_cache = {}
+    return svn_metadata_cache
+
+
+def backup_svn_metadata(svn_metadata_cache, start_cwd, svncachefile, start_timestamp_string):
+    ## @brief Persistify SVN metadata cache in JSON format
+    #  @param svn_metadata_cache SVN metadata cache
+    #  @param start_cwd Directory to change to before dumping
+    #  @param start_timestamp_string Timestamp backup for previous version of the cache
+    os.chdir(start_cwd)
+    if os.path.exists(svncachefile):
+        os.rename(svncachefile, svncachefile+".bak."+start_timestamp_string)
+    with file(svncachefile, "w") as md_dump:
+        json.dump(svn_metadata_cache, md_dump, indent=2)
 
