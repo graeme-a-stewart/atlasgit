84a53704b5ddc733b2c572832e82aa7e1d2a33ab
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2017-01-12 23:16:17 +0100

    Improve parsing of base releases
    Use perl to get only 3 digit releases in most cases

--- import.sh
+++ import.sh
@@ -6,19 +6,22 @@
 for r in 20.8 21.0; do
     cmaketags.py $r
 done
-for r in 19.2 20.1 20.7 20.8; do
+for r in 19.2 20.1 20.7 20.8 20.11; do
     cmttags.py $r
 done
 
+# Copy definitive author list...
+cp ~/bin/aogt.author.metadata .
+
 # Import all tags
-(time asvn2git.py file:///atlas/scratch0/graemes/ao-mirror aogt tagdir/* --licensefile ~/bin/apache2.txt --uncrustify ~/bin/uncrustify-import.cfg) |& tee o.agot.a2s
+(time asvn2git.py file:///data/graemes/atlasoff/ao-mirror aogt $(ls -v tagdir/* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --licensefile ~/bin/apache2.txt) |& tee o.agot.a2s
 
 # Build master branch
-(time branchbuilder.py aogt master $(ls -v tagdir/19.2.? tagdir/20.1.? tagdir/20.11.? tagdir/20.7.? tagdir/20.8.? tagdir/21.0.*) --skipreleasetag --onlyforward) |& tee o.aogt.master
+(time branchbuilder.py aogt master $(ls -v tagdir/* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --skipreleasetag --onlyforward) |& tee o.aogt.master
 
 # Build release branches
 for r in 19.2 20.1 20.7 20.8 21.0; do
-    (time branchbuilder.py aogt $r $(ls -v tagdir/$r.?) --parentbranch master:@$(pwd)/tagdir/$r.0 ) |& tee o.aogt.bb.$r
+    (time branchbuilder.py aogt $r $(ls -v tagdir/$r.* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --parentbranch master:@$(pwd)/tagdir/$r.0 ) |& tee o.aogt.bb.$r
 done
 # HLT branch was made from 20.7, not dev
 (time branchbuilder.py aogt 20.11 $(ls -v tagdir/20.11.?) --parentbranch 20.7:@$(pwd)/tagdir/20.11.0 ) |& tee o.aogt.bb.20.11
