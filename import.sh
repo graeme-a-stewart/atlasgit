25c217e5f53899cc1d578ea56e344ba5f1f5b376
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2017-01-14 12:22:58 +0100

    Update import to be more flexible in target git repo
    It’s a good idea to be easily able to import multiple times
    to check different settings

--- import.sh
+++ import.sh
@@ -2,6 +2,8 @@
 #
 # Top to bottom import of ATLAS SVN to git
 
+gitrepo=${1:-aogt}
+
 # Get package tags
 for r in 20.8 21.0; do
     cmaketags.py $r
@@ -9,22 +11,23 @@ done
 for r in 19.2 20.1 20.7 20.8 20.11; do
     cmttags.py $r
 done
+rm tagdir/21.0.56  # What was that?
 
 # Copy definitive author list...
-cp ~/bin/aogt.author.metadata .
+cp ~/bin/$gitrepo.author.metadata .
 
 # Import all tags
-(time asvn2git.py file:///data/graemes/atlasoff/ao-mirror aogt $(ls -v tagdir/* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --licensefile ~/bin/apache2.txt) |& tee o.agot.a2s
+(time asvn2git.py file:///data/graemes/atlasoff/ao-mirror $gitrepo $(ls -v tagdir/* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --licensefile ~/bin/apache2.txt) |& tee o.agot.a2s
 
 # Build master branch
-(time branchbuilder.py aogt master $(ls -v tagdir/* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --skipreleasetag --onlyforward) |& tee o.aogt.master
+(time branchbuilder.py $gitrepo master $(ls -v tagdir/* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --skipreleasetag --onlyforward) |& tee o.$gitrepo.master
 
 # Build release branches
 for r in 19.2 20.1 20.7 20.8 21.0; do
-    (time branchbuilder.py aogt $r $(ls -v tagdir/$r.* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --parentbranch master:@$(pwd)/tagdir/$r.0 ) |& tee o.aogt.bb.$r
+    (time branchbuilder.py $gitrepo $r $(ls -v tagdir/$r.* | perl -ne 'print if /\/\d+\.\d+\.\d+$/') --parentbranch master:@$(pwd)/tagdir/$r.0 ) |& tee o.$gitrepo.bb.$r
 done
 # HLT branch was made from 20.7, not dev
-(time branchbuilder.py aogt 20.11 $(ls -v tagdir/20.11.?) --parentbranch 20.7:@$(pwd)/tagdir/20.11.0 ) |& tee o.aogt.bb.20.11
+(time branchbuilder.py $gitrepo 20.11 $(ls -v tagdir/20.11.?) --parentbranch 20.7:@$(pwd)/tagdir/20.11.0 ) |& tee o.$gitrepo.bb.20.11
 
 
 # Update for master:
